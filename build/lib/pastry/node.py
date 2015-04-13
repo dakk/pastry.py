86376b768dcc69cc3691d0f4702e6f657925335d
Davide Gessa <gessadavide@gmail.com>
2015-04-13 17:19:54 +0200

    debug and bootstrap

--- build/lib/pastry/node.py
+++ build/lib/pastry/node.py
@@ -1,15 +1,49 @@
 from threading import Thread
 from threading import Lock
+from colorlog import ColoredFormatter
 import logging
 
-logging.basicConfig(format='[%(asctime)-8s] %(module)s - %(message)s', level=logging.DEBUG)
+
+formatter = ColoredFormatter(
+	'%(log_color)s[%(asctime)-8s] %(module)s (%(process)d %(threadName)s): %(message_log_color)s%(message)s',
+	datefmt=None,
+	reset=True,
+	log_colors={
+		'DEBUG':    'blue',
+		'INFO':     'green',
+		'WARNING':  'yellow',
+		'ERROR':    'red',
+		'CRITICAL': 'red',
+	},
+	secondary_log_colors={
+		'message': {
+			'DEBUG':    'purple',
+			'INFO':     'yellow',
+			'WARNING':  'green',
+			'ERROR':    'yellow',
+			'CRITICAL': 'red',
+		}
+	},
+	style = '%'
+)
+
+stream = logging.StreamHandler()
+stream.setFormatter(formatter)
+
+logger = logging.getLogger('pastry.py')
+logger.addHandler(stream)
+logger.setLevel (logging.DEBUG)
+
 
 class Node:
 	def __init__ (self, port, auth):
-		logging.debug ('Node initialized')
+		self.port = port
+		self.auth = auth
+		logger.debug ('listening on port %d', self.port)
 
 	def bootstrap (self, seeds = []):
-		pass
+		for seed in seeds:
+			logger.debug ('bootstraping from %s', str (seed))
 
 	def get (self, key):
 		pass
--- examples/node_example.py
+++ examples/node_example.py
@@ -2,3 +2,4 @@ from pastry import node
 
 
 pastry_node = node.Node (8484, None)
+pastry_node.bootstrap ([('127.0.0.1', 8485), ('127.0.0.1', 8486), ('127.0.0.1', 8487)])
