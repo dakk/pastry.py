8cd7c31fbac9c13966e0003770f0f29ebd4ba27d
Davide Gessa <gessadavide@gmail.com>
2015-04-13 16:21:25 +0200

    pastry node

---
+++ pastry/node.py
@@ -0,0 +1,19 @@
+from threading import Thread
+from threading import Lock
+
+class Node:
+	def __init__ (self, port):
+		pass
+
+	def bootstrap (self, seeds = []):
+		pass
+
+	def get (self, key):
+		pass
+
+	def set (self, key, data):
+		pass
+
+
+	
+	
---
+++ setup.py
@@ -0,0 +1,10 @@
+
+from distutils.core import setup
+
+setup(name='pastry',
+      version='0.1',
+      description='Pastry DHT python implementation',
+      author='Davide Gessa',
+      author_email='gessadavide@gmail.com',
+      packages=['pastry'],
+     )
